d01b7a74be1ea30e517040994ccb27a7fc59e04b
Saad T <tariqs26@mcmaster.ca>
2023-02-03 13:32:19 -0500

    add geometry to schema

--- src/models/campground.js
+++ src/models/campground.js
@@ -4,6 +4,17 @@ import Review from './review.js';
 const CampgroundSchema = new Schema({
   title: String,
   image: String,
+  geometry: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
   price: Number,
   description: String,
   location: String,
