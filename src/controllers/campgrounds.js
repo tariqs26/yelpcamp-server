90e5e475c66d7d7bdace277c40e35dc43a961b25
Saad T <tariqs26@mcmaster.ca>
2023-02-03 13:32:39 -0500

    add map logic

--- src/controllers/campgrounds.js
+++ src/controllers/campgrounds.js
@@ -1,12 +1,17 @@
 import Campground from '../models/campground.js';
 import ExpressError from '../utils/ExpressError.js';
+import { config } from 'dotenv';
+import mbxGeocoding from '@mapbox/mapbox-sdk/services/geocoding.js';
+
+if (process.env.NODE_ENV !== 'production') config();
+
+const geocoder = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 
 export const index = async (_, res) => {
   // const { page = 1 } = req.query;
   // const MAX_CAMPGROUNDS = 10;
 
-  res.send(await Campground.find({})
-  );
+  res.send(await Campground.find({}));
 };
 
 export const getCampground = async (req, res) => {
@@ -18,6 +23,7 @@ export const getCampground = async (req, res) => {
       populate: { path: 'author' },
     });
     const populateAuthor = await populateReviews.populate('author');
+
     res.send(populateAuthor);
   } catch (e) {
     throw new ExpressError('Campground not found', 404);
@@ -25,7 +31,13 @@ export const getCampground = async (req, res) => {
 };
 
 export const createCampground = async (req, res) => {
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.location,
+    })
+    .send();
   const campground = new Campground(req.body);
+  campground.geometry = geoData.body.features[0].geometry;
   campground.author = req.user._id;
   await campground.save();
   res.send(campground);
@@ -33,6 +45,12 @@ export const createCampground = async (req, res) => {
 
 export const updateCampground = async (req, res) => {
   const { id } = req.params;
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.location,
+    })
+    .send();
+  req.body.geometry = geoData.body.features[0].geometry;
   await Campground.findByIdAndUpdate(id, req.body, {
     runValidators: true,
     new: true,
