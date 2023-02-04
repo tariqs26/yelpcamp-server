5a9940d14171ed0be226d2bf175c9f8e67adffe5
Saad T <tariqs26@mcmaster.ca>
2023-02-04 07:12:01 -0500

    pass error to middleware, fix app crash

--- src/middlewares/userAuthorized.js
+++ src/middlewares/userAuthorized.js
@@ -5,7 +5,6 @@ export default async function (req, _, next) {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (!campground.author.equals(req.user._id) && !req.user.isAdmin)
-    throw new ExpressError('You do not have permission to do that', 403);
-
+    next(new ExpressError('You do not have permission to do that', 403));
   next();
 }
