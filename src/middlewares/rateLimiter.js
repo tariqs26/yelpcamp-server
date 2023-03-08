f011b528f32d9f2bae16298890ebe295dfa1b5c9
Saad T <tariqs26@mcmaster.ca>
2023-03-08 02:42:49 -0500

    update rate limit restrictions

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -2,14 +2,14 @@ import { rateLimit } from 'express-rate-limit';
 
 export const accountCreationLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 1 day window
-  max: 2, // start blocking after 2 requests
+  max: 5, // start blocking after 5 requests
   message:
     'Too many accounts created from this IP, please try again after a day',
 });
 
 export const createCampgroundLimiter = rateLimit({
-  windowMs: 5 * 60 * 60 * 1000, // 5 hour window
+  windowMs: 5 * 60 * 60 * 1000, // 1 hour window
   max: 5, // start blocking after 5 requests
   message:
-    'Too many campgrounds created from this IP, please try again after 5 hours',
+    'Too many campgrounds created from this IP, please try again after an hour',
 });
