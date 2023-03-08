dfc44285fff5e73d493be090420e49a41bc4f4eb
Saad T <tariqs26@mcmaster.ca>
2023-03-08 02:49:12 -0500

    skip failed requests is enabled

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -3,13 +3,15 @@ import { rateLimit } from 'express-rate-limit';
 export const accountCreationLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 1 day window
   max: 5, // start blocking after 5 requests
+  skipFailedRequests: true,
   message:
     'Too many accounts created from this IP, please try again after a day',
 });
 
 export const createCampgroundLimiter = rateLimit({
   windowMs: 1 * 60 * 60 * 1000, // 1 hour window
-  max: 5, // start blocking after 5 requests
+  max: 1, // start blocking after 5 requests
+  skipFailedRequests: true,
   message:
     'Too many campgrounds created from this IP, please try again after an hour',
 });
