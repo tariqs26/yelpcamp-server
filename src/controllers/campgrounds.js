321b6cca265253ab114a28f82189082ec87f434b
Saad T <tariqs26@mcmaster.ca>
2023-03-08 03:24:31 -0500

    fix:campground get

--- src/controllers/campgrounds.js
+++ src/controllers/campgrounds.js
@@ -7,19 +7,18 @@ if (process.env.NODE_ENV !== 'production') config();
 
 const geocoder = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 
-export const index = async (_, res) => {
+export const index = async (req, res) => {
   // const { page = 1 } = req.query;
-  // const MAX_CAMPGROUNDS = 10;
-  res.send(
-    {
-      // campgrounds: await Campground.find({})
-      //   .skip((page - 1) * MAX_CAMPGROUNDS)
-      //   .limit(MAX_CAMPGROUNDS),
-      // totalPages: Math.ceil(
-      //   (await Campground.countDocuments()) / MAX_CAMPGROUNDS
-      // ),
-    }
-  )
+  // const MAX_CAMPGROUNDS = 5;
+  // res.send({
+  //   campgrounds: await Campground.find({})
+  //     .sort({ createdAt: -1 })
+  //     .skip((page - 1) * MAX_CAMPGROUNDS)
+  //     .limit(MAX_CAMPGROUNDS),
+  //   totalPages: Math.ceil(
+  //     (await Campground.countDocuments()) / MAX_CAMPGROUNDS
+  //   ),
+  // });
 
   res.send(await Campground.find({}));
 };
