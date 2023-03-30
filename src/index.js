5d0f8419b8aea33205b88aefc70cc3fea2f65fa9
Saad T <tariqs26@mcmaster.ca>
2023-03-30 18:05:44 -0400

    document file

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ import User from './models/user.js';
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
+// Set up mongoose connection
 set('strictQuery', true);
 connect(dbUrl, {
   useNewUrlParser: true,
@@ -30,19 +31,24 @@ db.once('open', () => {
 });
 
 const app = express();
+
+// encoding incoming data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set up CORS
 app.use(
   cors({
     origin: process.env.CLIENT_ORIGIN,
     credentials: true,
   })
 );
-app.use(helmet());
-app.use(mongoSanitize());
+app.use(helmet()); // set security HTTP headers
+app.use(mongoSanitize()); // prevent NoSQL injection
 
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
+// Set up session store
 const store = new MongoDBStore({
   mongoUrl: dbUrl,
   secret,
@@ -52,7 +58,8 @@ const store = new MongoDBStore({
 store.on('error', function (e) {
   console.log('SESSION STORE ERROR', e);
 });
-app.set('trust proxy', 1);
+app.set('trust proxy', 1); // trust first proxy
+// Set up session
 app.use(
   session({
     store,
@@ -69,25 +76,31 @@ app.use(
     },
   })
 );
+
+// Set up passport for authentication
 app.use(passport.initialize(), passport.session());
 passport.use(new passportLocal(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Set up routes
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 app.use('/', userRoutes);
 
+// For routes that don't exist
 app.all('*', (_, __, next) => {
   next(new ExpressError('Page not found', 404));
 });
 
+// Set up error handler
 app.use((err, _, res, __) => {
   res.status(err.status || 500).send(err.message);
 });
 
 const port = process.env.PORT || 3000;
 
+// Enable server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
