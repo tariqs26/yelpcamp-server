e0340528eab295491ff9e14bcf5dd8140b651cb5
Saad Tariq <tariqs26@mcmaster.ca>
2023-07-06 03:01:23 -0400

    move catch to utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,6 @@
+import type { Request, Response, NextFunction } from "express"
+
+export const catchAsync =
+  (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next)
+  }
--- src/utils/ExpressError.js
+++
@@ -1,6 +0,0 @@
-export default class ExpressError extends Error {
-  constructor(message, status) {
-    super(message);
-    this.status = status;
-  }
-}
