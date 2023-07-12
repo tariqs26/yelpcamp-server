0bfd39c512a6a3c412cf59548f088a015fe24e26
Saad Tariq <tariqs26@mcmaster.ca>
2023-07-11 23:53:10 -0400

    use throw, simplify boolean logic

--- src/middlewares/user-authorized.ts
+++ src/middlewares/user-authorized.ts
@@ -9,8 +9,8 @@ export default async function userAuthorized(
 ) {
   const { id } = req.params
   const campground = await Campground.findById(id)
-  if (!campground) return next(new ExpressError("Campground not found", 404))
-  if (!campground.author.equals(req.user?._id) && !req.user?.isAdmin)
-    return next(new ExpressError("You do not have permission to do that", 403))
+  if (!campground) throw new ExpressError("Campground not found", 404)
+  if (!(campground.author.equals(req.user?._id) || req.user?.isAdmin))
+    throw new ExpressError("You do not have permission to do that", 403)
   next()
 }
