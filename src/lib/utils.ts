f9fa95ef63c09a6b8d0d51a1ece2cfd68c6a584b
Saad Tariq <saadtariq.dev@outlook.com>
2024-02-23 11:38:58 -0500

    refactor: update function type, explicit undefined check

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,13 +2,14 @@ import type { Request, Response, NextFunction } from "express"
 import { BadRequestError } from "./exceptions"
 
 export const catchAsync =
-  (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch(next)
-  }
+  (fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) =>
+    (req: Request, res: Response, next: NextFunction) => {
+      fn(req, res, next).catch(next)
+    }
 
 export const getParamsId = (req: Request) => {
   const { id } = req.params
-  if (!id) throw new BadRequestError("Missing id")
+  if (id === undefined) throw new BadRequestError("Missing id")
   if (typeof id !== "string") throw new BadRequestError("Invalid id")
   return id
 }
