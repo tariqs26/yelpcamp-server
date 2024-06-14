5c485b5a6af7cd3ddd2d3d50c1f525fa37b07fa8
Saad Tariq <saadtariq.dev@outlook.com>
2024-06-13 23:59:58 -0400

    refactor: update type for NODE_ENV to use enum

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,15 +1,17 @@
 import dotenv from "dotenv"
-import Z from "zod"
+import { z } from "zod"
 
 if (process.env.NODE_ENV !== "production") dotenv.config()
 
-const envSchema = Z.object({
-  NODE_ENV: Z.string().optional(),
-  PORT: Z.string().optional(),
-  DATABASE_URL: Z.string().min(1),
-  CLIENT_ORIGIN: Z.string().min(1),
-  MAPBOX_TOKEN: Z.string().min(1),
-  SECRET: Z.string().min(1)
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  PORT: z.string().optional(),
+  DATABASE_URL: z.string().min(1),
+  CLIENT_ORIGIN: z.string().min(1),
+  MAPBOX_TOKEN: z.string().min(1),
+  SECRET: z.string().min(1),
 })
 
 export const env = envSchema.parse(process.env)
