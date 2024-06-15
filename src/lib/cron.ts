ade20876a2128d9ca0ca40d29aac507fcd0df1e1
Saad Tariq <saadtariq.dev@outlook.com>
2024-06-14 20:03:51 -0400

    refactor: update cron schedule to run every 14 minutes instead of 15

--- src/lib/cron.ts
+++ src/lib/cron.ts
@@ -1,7 +1,7 @@
 import cron from "node-cron"
 
 export const task = cron.schedule(
-  "*/15 * * * *",
+  "*/14 * * * *",
   () => {
     console.log("Pinging server...")
     const url = "https://yc-server8080.onrender.com/ping"
