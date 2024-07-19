18f698ef6842ef5e56c79c3198cab2cae865b80c
Saad Tariq <saadtariq.dev@outlook.com>
2024-07-19 17:35:31 -0400

    refactor: update error handling middleware

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,5 +1,4 @@
 import type { NextFunction, Request, Response } from "express"
-import { MongooseError } from "mongoose"
 
 export default function errorHandler(
   error: {
@@ -12,16 +11,8 @@ export default function errorHandler(
 ) {
   if (res.headersSent) return next(error)
 
-  let status = error.status ?? 500
-  let message = error.message ?? "Something went wrong"
-  if (error instanceof MongooseError) {
-    switch (error.name) {
-      case "CastError":
-        status = 400
-        message = "Invalid ObjectId"
-        break
-    }
-  }
+  const status = error.status ?? 500
+  const message = error.message ?? "Something went wrong"
 
   res.status(status).send(message)
 }
