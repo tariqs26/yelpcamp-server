4f895f5753af85740c44aef8b8f68e509b5aecb0
Saad Tariq <saadtariq.dev@outlook.com>
2024-10-21 15:17:41 -0400

    refactor: update userAuthenticated middleware to use req.isUnauthenticated()

--- src/middlewares/user-authenticated.ts
+++ src/middlewares/user-authenticated.ts
@@ -2,6 +2,6 @@ import type { RequestHandler } from "express"
 import { NotAuthenticatedError } from "../lib/exceptions"
 
 export const userAuthenticated: RequestHandler = (req, _res, next) => {
-  if (req.user === undefined) return next(new NotAuthenticatedError())
+  if (req.isUnauthenticated()) return next(new NotAuthenticatedError())
   next()
 }
