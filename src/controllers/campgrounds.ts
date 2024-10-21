58ed57cac7366b5db5b721ddb07f026645d5fb8e
Saad Tariq <saadtariq.dev@outlook.com>
2024-10-21 13:27:29 -0400

    refactor: send total campgrounds count

--- src/controllers/campgrounds.ts
+++ src/controllers/campgrounds.ts
@@ -9,23 +9,23 @@ import Campground from "../models/campground"
 export const getCampgrounds = async (req: Request, res: Response) => {
   const { page = 1 } = req.query
   const PER_PAGE = 5
+  const total = await Campground.countDocuments()
 
   res.send({
+    total,
+    totalPages: Math.ceil(total / PER_PAGE),
     campgrounds: await Campground.find({})
       .sort({ createdAt: -1 })
       .skip((Number(page) - 1) * PER_PAGE)
       .limit(PER_PAGE),
-    totalPages: Math.ceil((await Campground.countDocuments()) / PER_PAGE),
   })
 }
 
 export const createCampground = async (req: Request, res: Response) => {
   const body = validate(req.body, campgroundSchema)
-
   const geometry = await getGeoDataGeometry(body.location)
 
   const campground = new Campground(body)
-
   campground.geometry = geometry
   campground.author = req.user?._id
 
@@ -36,7 +36,6 @@ export const createCampground = async (req: Request, res: Response) => {
 
 export const getCampground = async (req: Request, res: Response) => {
   const campground = await Campground.findById(getParamsId(req))
-
   if (!campground) throw new NotFoundError("Campground")
 
   const populateReviews = await campground.populate({
@@ -49,7 +48,6 @@ export const getCampground = async (req: Request, res: Response) => {
 
 export const updateCampground = async (req: Request, res: Response) => {
   const body = validate(req.body, campgroundSchema)
-
   const geometry = await getGeoDataGeometry(body.location)
 
   const campground = await Campground.findByIdAndUpdate(
@@ -57,7 +55,6 @@ export const updateCampground = async (req: Request, res: Response) => {
     { ...body, geometry },
     { new: true }
   )
-
   if (!campground) throw new NotFoundError("Campground")
 
   res.json("Campground updated successfully")
@@ -65,5 +62,6 @@ export const updateCampground = async (req: Request, res: Response) => {
 
 export const deleteCampground = async (req: Request, res: Response) => {
   await Campground.findByIdAndDelete(getParamsId(req))
+  
   res.json("Campground deleted successfully")
 }
