a2be0875209f0aa18f6f97a4fbcccd837f4db922
Saad Tariq <saadtariq.dev@outlook.com>
2024-10-23 16:02:47 -0400

    refactor: throw NotAuthenticatedError directly in userAuthenticated middleware

--- src/middlewares/user-authenticated.ts
+++ src/middlewares/user-authenticated.ts
@@ -2,6 +2,6 @@ import type { RequestHandler } from "express"
 import { NotAuthenticatedError } from "../lib/exceptions"
 
 export const userAuthenticated: RequestHandler = (req, _res, next) => {
-  if (req.isUnauthenticated()) return next(new NotAuthenticatedError())
+  if (req.isUnauthenticated()) throw new NotAuthenticatedError()
   next()
 }
