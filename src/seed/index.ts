157dbe4ef91d2d178316feef3e07539ced0a6857
Saad Tariq <saadtariq.dev@outlook.com>
2025-02-08 21:12:38 -0500

    refactor: improve database connection log message

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -11,7 +11,7 @@ connect(env.DATABASE_URL).catch((error) => {
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "connection error:"))
 db.once("open", () => {
-  console.log("Database connected")
+  console.log("Database connected successfully")
 })
 
 const sample = (array: string[]) =>
