eb755f842c3c4d56943121f42217f32f5dae3c3d
Saad Tariq <saadtariq.dev@outlook.com>
2025-03-17 22:46:33 -0400

    refactor: improve validation for URL based env variables, add default value for `PORT`

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,9 +7,9 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default("development"),
-  PORT: z.string().optional(),
-  DATABASE_URL: z.string().min(1),
-  CLIENT_ORIGIN: z.string().min(1),
+  PORT: z.string().default("3000"),
+  DATABASE_URL: z.string().url(),
+  CLIENT_ORIGIN: z.string().url(),
   MAPBOX_TOKEN: z.string().min(1),
   SECRET: z.string().min(1),
 })
